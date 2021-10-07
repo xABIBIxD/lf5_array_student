bdd62a4f83145fc473c0bbccf2a0fece591ddce1
xABIBIx <abdelouadoud1@schule.bremen.de>
2021-10-07 15:24:30 +0200

    30.09.21

--- src/arraySum2d/ArraySum2d.java
+++ src/arraySum2d/ArraySum2d.java
@@ -2,20 +2,7 @@ package arraySum2d;
 
 public class ArraySum2d {
     public int sumAll(int[][] array) throws IllegalArgumentException {
-        if (array==null){
-            throw new IllegalArgumentException();
-        }
-        int summe =0;
-        for (int i = 0;i< array.length;i++){
-            for (int j =0;j<array[i].length;j++){
-                summe = summe +array[i][j];
-            } // Tolga = Messi
-        }
-        return summe;
-    }
-
-    public int[] rowSums(int[][] array) throws IllegalArgumentException {
-        if (array==null){
+        if (array==null || array.length==0 || array[0].length==0){
             throw new IllegalArgumentException();
         }
         int summe =0;
@@ -24,10 +11,49 @@ public class ArraySum2d {
                 summe = summe +array[i][j];
             }
         }
-        return null;
+        return summe;
+    }
+
+    public int[] rowSums(int[][] array) throws IllegalArgumentException {
+        if (array==null || array.length==0 || array[0].length==0){
+            throw new IllegalArgumentException();
+        }
+        int []zwisch= new int[array.length];
+        for (int i = 0;i< array.length;i++){
+            for (int j =0;j<array[i].length;j++){
+                zwisch[i]+=array[i][j];
+            }
+        }
+        return zwisch;
     }
 
     public int[] colSums(int[][] array) throws IllegalArgumentException {
-        return null;
+        if (array==null || array.length==0 || array[0].length==0){
+            throw new IllegalArgumentException();
+        }
+
+        int []zwisch= new int[array[0].length];
+        for (int y = 0;y< array.length;y++){
+            for (int x =0;x<array[y].length;x++){
+                zwisch[x]+=array[y][x];
+            }
+        }
+
+        /*
+        array --->
+        4 | 1 | 2 | 5
+        3 | 1 | 2 | 2
+        7 | 1 | 1 | 1
+
+
+        int[] row=array[1];  --> 3 | 1 | 2 | 2
+
+        int value=row[2];  --> 2
+
+        value = array[1][2] --> 2
+        */
+
+        return zwisch;
     }
+
 }
